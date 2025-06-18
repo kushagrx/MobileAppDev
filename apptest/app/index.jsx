f3a34a62c098ddd1f87174fddcd431aa8b0e07d0
kushagrabisht05 <kushagrabisht10@gmail.com>
2025-06-18 22:56:55 +0530

    few changes in tailwindcss

--- apptest/app/index.jsx
+++ apptest/app/index.jsx
@@ -1,9 +1,12 @@
+import "./global.css"
 import { Text, View } from "react-native";
-
-export default function Index() {
+ 
+export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Lets finally get going.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
