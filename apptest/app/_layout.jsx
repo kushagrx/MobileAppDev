2a6455ea3d555eebebdcdb22df5656e30260f6a7
kushagrabisht05 <kushagrabisht10@gmail.com>
2025-06-25 12:55:25 +0530

    basic navigation and styling

--- apptest/app/_layout.jsx
+++ apptest/app/_layout.jsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return(
+    <Stack>
+      <Stack.Screen name="index" options={{title:"Landing page"}}/>
+      <Stack.Screen name="login" options={{title:"Login"}}/>
+    </Stack>
+  );
 }
--- apptest/app/index.jsx
+++ apptest/app/index.jsx
@@ -1,12 +1,26 @@
-import "./global.css"
-import { Text, View } from "react-native";
- 
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl font-bold text-blue-500">
         Welcome!
       </Text>
+      <Link href="/login" style={styles.navButton}>Login page</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  navButton: {
+    width: 150,
+    backgroundColor: "coral",
+    borderRadius: 8,
+    height: 30,
+    fontSize: 25,
+    textAlign: "center",
+    display: "flex",
+    justifyContent: "center"
+  }
+});
