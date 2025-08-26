1c6b9c8197ae38de2d5fade2a510bc53c6d656f9
kushagrx <kushagrabisht10@gmail.com>
2025-08-26 13:57:37 +0530

    created a new group to test navigation

--- reactnative5/app/(coffee)/index.jsx
+++ reactnative5/app/(coffee)/index.jsx
@@ -1,7 +1,8 @@
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, Pressable} from 'react-native'
 import React from 'react'
 import icedcoffee from '@/assets/images/icedcoffee.jpg'
 import { ImageBackground } from 'expo-image'
+import { Link } from 'expo-router'
 
 export default function app() {
   return (
@@ -11,7 +12,13 @@ export default function app() {
       style={styles.image}
       >
       <Text style={styles.text}>Welcome To My Coffee Shop!</Text>
+      <Link href="/explore" style={{marginHorizontal:'auto'}}>
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Explore</Text>
+      </Pressable>
+      </Link>
       </ImageBackground>
+
     </View>
   )
 }
@@ -27,7 +34,29 @@ const styles=StyleSheet.create({
     fontWeight:'bold',
     textAlign:'center',
     backgroundColor:'#000000a0',
+  }, 
+  link:{
+    color:'white',
+    fontSize:42,
+    fontWeight:'bold',
+    textAlign:'center',
+    backgroundColor:'#000000a0',
+    marginBottom:20,
   },
+  button:{
+    height:60,
+    borderRadius:20,
+    justifyContent:'center',
+    backgroundColor:'rgba(0,0,0,0.75)',
+    padding:4
+  },
+  buttonText:{
+    color:'white',
+    fontSize:16,
+    fontWeight:'bold',
+    textAlign:'center',
+    padding:2,
+  }, 
   image:{
     width:'100%',
     height:'100%',
